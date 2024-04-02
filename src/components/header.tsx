0d88f02e9c10cd3eb445db07ac778e80842b8de9
Sir_46 <fadlan.8291@gmail.com>
2024-04-02 21:43:06 +0700

    Fixed SEO and Favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,7 @@ import Link from 'next/link';
 import ThemeToggle from './theme-toggle';
 import { buttonVariants } from './ui/button';
 import { cn } from '@/lib/utils';
+import { Icons } from './icons';
 
 type Props = {};
 
@@ -17,8 +18,12 @@ const Header = (props: Props) => {
   return (
     <header className='fixed top-0 left-0 right-0 bg-background z-20'>
       <div className='container h-16 flex items-center justify-between'>
-        <Link href='/' className='text-lg font-bold'>
-          Fadlan.dev
+        <Link
+          href='/'
+          className='text-lg font-bold flex items-center space-x-2'
+        >
+          <Icons.logo className='h-6 w-6' />
+          <span className='font-bold'>Fadlan.dev</span>
         </Link>
         <div className='flex items-center'>
           <Link
