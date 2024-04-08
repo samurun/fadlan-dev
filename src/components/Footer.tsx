d785e9d23444e6aaac1443c11bf5a6f8cc6d4455
Sir_46 <fadlan.8291@gmail.com>
2024-04-08 16:29:47 +0700

    Modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,11 @@
+import {
+  EnvelopeClosedIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from '@radix-ui/react-icons';
 import NowPlaying from './now-playing';
+import { buttonVariants } from './ui/button';
+import { cn } from '@/lib/utils';
 
 type Props = {};
 
@@ -7,8 +14,41 @@ const Footer = (props: Props) => {
     <footer className='pt-2 pb-14 border-t mt-11'>
       <div className='container'>
         <NowPlaying />
-        <div className='text-muted-foreground'>
-          <p>© 2024 Fadlan</p>
+        <div className='flex justify-between'>
+          <div className='text-muted-foreground'>
+            <p>© 2024 Fadlan</p>
+          </div>
+          <div>
+            <a
+              href='mailto:fadlan.jehteerokee@gmail.com'
+              className={cn(
+                buttonVariants({ variant: 'ghost', size: 'icon' }),
+                'rounded-full'
+              )}
+            >
+              <EnvelopeClosedIcon />
+            </a>
+            <a
+              href='www.linkedin.com/in/fadlan-jehteerokee'
+              target='_blank'
+              className={cn(
+                buttonVariants({ variant: 'ghost', size: 'icon' }),
+                'rounded-full'
+              )}
+            >
+              <LinkedInLogoIcon />
+            </a>
+            <a
+              href='https://github.com/fadlan-dev'
+              target='_blank'
+              className={cn(
+                buttonVariants({ variant: 'ghost', size: 'icon' }),
+                'rounded-full'
+              )}
+            >
+              <GitHubLogoIcon />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
