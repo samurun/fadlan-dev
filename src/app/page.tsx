ab84d7fb88772513d1122776df80465879c7eb0b
Sir_46 <fadlan.8291@gmail.com>
2024-07-20 14:07:00 +0700

    refactor: Move LatestPosts to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import { posts } from '#site/content';
 import Hero from '@/components/hero';
-import PostItem from '@/components/post-item';
-import ProjectsSection from '@/components/projects-section';
+import LatestProjects from '@/components/latest-projects';
 import SummarizeMonthActivities from '@/components/summarize-month-activities';
-import { Button } from '@/components/ui/button';
 import { siteConfig } from '@/config/site';
-import { sortPosts } from '@/lib/utils';
-import { ArrowRightIcon } from '@radix-ui/react-icons';
-import Link from 'next/link';
+import LatestPosts from '@/components/latest-posts';
 
 export async function generateMetadata() {
   return {
@@ -38,35 +33,11 @@ export async function generateMetadata() {
 }
 
 export default function Home() {
-  const lastePosts = sortPosts(posts.filter((post) => post.published)).slice(
-    0,
-    3
-  );
   return (
     <div>
       <Hero />
-      <ProjectsSection />
-      <section className='py-28'>
-        <h2 className='text-3xl font-black'>Latest Posts</h2>
-        <ul className='flex flex-col gap-4 mt-8'>
-          {lastePosts.map((post) => (
-            <li key={post.slug}>
-              <PostItem
-                slug={post.slug}
-                title={post.title}
-                description={post.description}
-                date={post.date}
-                tags={post.tags}
-              />
-            </li>
-          ))}
-        </ul>
-        <Link href='/blog'>
-          <Button variant='outline' className='mt-16'>
-            View all blog <ArrowRightIcon className='ml-2' />
-          </Button>
-        </Link>
-      </section>
+      <LatestProjects />
+      <LatestPosts />
       <SummarizeMonthActivities />
     </div>
   );
---
+++ src/components/latest-posts.tsx
@@ -0,0 +1,40 @@
+import { posts } from '#site/content';
+import { sortPosts } from '@/lib/utils';
+import PostItem from './post-item';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
+
+type Props = {};
+
+const LatestPosts = async (props: Props) => {
+  const lastePosts = sortPosts(posts.filter((post) => post.published)).slice(
+    0,
+    3
+  );
+  return (
+    <section className='py-28'>
+      <h2 className='text-3xl font-black'>Latest Posts</h2>
+      <ul className='flex flex-col gap-4 mt-8'>
+        {lastePosts.map((post) => (
+          <li key={post.slug}>
+            <PostItem
+              slug={post.slug}
+              title={post.title}
+              description={post.description}
+              date={post.date}
+              tags={post.tags}
+            />
+          </li>
+        ))}
+      </ul>
+      <Link href='/blog'>
+        <Button variant='outline' className='mt-16'>
+          View all blog <ArrowRightIcon className='ml-2' />
+        </Button>
+      </Link>
+    </section>
+  );
+};
+
+export default LatestPosts;
